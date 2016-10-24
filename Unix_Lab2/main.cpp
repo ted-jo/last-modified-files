4937cd3087b21c722ca364b7e4a06321d4ed3edf
Ted Johansmeyer <Teddyjo@Teds-MacBook-Pro.local>
2016-10-24 00:11:18 -0400

    Initial Commit

---
+++ Unix_Lab2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Unix_Lab2
+//
+//  Created by Ted Johansmeyer on 10/24/16.
+//  Copyright © 2016 Ted Johansmeyer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
